07217c087bdd0fe9405341588656ebb450b4e93a
SamSamhuns <sthapuskar@yahoo.com>
2018-11-19 23:38:17 -0500

    Removed API_KEY_entry for all funcs as it is redundant when loaded as a module

--- src/modules/submit_cancel_orders.py
+++ src/modules/submit_cancel_orders.py
@@ -17,8 +17,8 @@ The requests.Session() object should deal with registering the API_KEY in the ma
 
 # submitting a market order
 # side = BUY/SELL
-def market_order( ses, ticker, side, quantity, API_KEY_entry ):
-    ses.headers.update(API_KEY)
+# function requires a requests.Session() object as the ses argument with a loaded API_KEY
+def market_order( ses, ticker, side, quantity):
     mkt_order_params = { 'ticker':ticker, 'type':'MARKET', 'quantity':quantity, 'action':side}
     response = ses.post( base_url+'/orders', params=mkt_order_params)
     if response.ok:
@@ -28,8 +28,8 @@ def market_order( ses, ticker, side, quantity, API_KEY_entry ):
     else:
         print('%s %s Market order was not submitted.' % (side, quantity))
 
-def limit_order( ses, ticker, side, quantity, price, API_KEY_entry ):
-    ses.headers.update(API_KEY)
+# function requires a requests.Session() object as the ses argument with a loaded API_KEY
+def limit_order( ses, ticker, side, quantity, price):
     lim_order_params = { 'ticker':ticker, 'type':'LIMIT', 'quantity':quantity, 'price':price, 'action':side}
     response = ses.post( base_url+'/orders', params=lim_order_params)
     if response.ok:
@@ -39,8 +39,8 @@ def limit_order( ses, ticker, side, quantity, price, API_KEY_entry ):
     else:
         print('%s %s Limit order was not submitted.' % (side, quantity))
 
-def cancel_order( ses, ticker, quantity, order_id, API_KEY_entry ):
-    ses.header.update(API_KEY)
+# function requires a requests.Session() object as the ses argument with a loaded API_KEY
+def cancel_order( ses, ticker, quantity, order_id ):
     response = ses.delete( (base_url+'/orders/{}').format(order_id))
     if response.ok:
         status = response.json()
@@ -52,8 +52,7 @@ def cancel_order( ses, ticker, quantity, order_id, API_KEY_entry ):
 # if all_flag = 1 then all open orders are cancelled
 # set all_flag = 0 to cancel only select orders
 # price_direc and volume_direc has a value of [ <, <=, >, >= or = ]
-def cancel_order_bulk( ses, all_flag, API_KEY_entry, price_direc, price_lim, volume_direc, volume_lim ):
-    ses.headers.update(API_KEY)
+def cancel_order_bulk( ses, all_flag, price_direc, price_lim, volume_direc, volume_lim ):
     # Volume < 0 for cancelling all open sell orders and Volume > 0for cancelling all open buy orders
     query_gen = 'Price' + price_direc + price_lim +'AND'+'Volume' + volume_direc + volume_lim
     cancel_params = {'all': all_flag, 'query': query_gen }
