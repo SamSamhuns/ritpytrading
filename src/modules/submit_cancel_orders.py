3eca6e740200ee95c96fa5387d4b38d53e3c5d55
SamSamhuns <sthapuskar@yahoo.com>
2018-11-23 16:26:46 -0500

    set default val for all_flag in cancel_order_bulk func.

--- src/modules/submit_cancel_orders.py
+++ src/modules/submit_cancel_orders.py
@@ -55,10 +55,12 @@ def cancel_order( ses, ticker, quantity, order_id ):
 
 # if all_flag = 1 then all open orders are cancelled
 # set all_flag = 0 to cancel only select orders
+# By default all_flag is set to 0
 # price_direc and volume_direc has a value of [ <, <=, >, >= or = ]
-def cancel_order_bulk( ses, all_flag, price_direc, price_lim, volume_direc, volume_lim ):
-    # Volume < 0 for cancelling all open sell orders and Volume > 0for cancelling all open buy orders
-    query_gen = 'Price' + price_direc + price_lim +'AND'+'Volume' + volume_direc + volume_lim
+def cancel_order_bulk( ses, price_direc, price_lim, volume_direc, volume_lim, all_flag=0 ):
+    # Volume < 0 for cancelling all open sell orders and Volume > 0 for cancelling all open buy orders
+    # query_gen example 'Price < 20.0 AND Volume > 0'
+    query_gen = 'Price ' + price_direc + ' ' + price_lim +' AND '+' Volume ' + ' ' + volume_direc + ' ' + volume_lim
     cancel_params = {'all': all_flag, 'query': query_gen }
     response = ses.post( base_url+'/commands/cancel', params=cancel_params)
     if response.ok:
