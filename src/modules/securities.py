5d81459fb2730478a6fb098e07b2899c72af05f0
SamSamhuns <sthapuskar@yahoo.com>
2018-11-24 10:50:34 -0500

    Added json params for securities http get module

--- src/modules/securities.py
+++ src/modules/securities.py
@@ -1,3 +1,47 @@
+'''
+The securities HTTP module gets a list of available securities
+and associated positions.
+
+securities object attribute values: JSON formatted
+[
+  {
+    "ticker": "string",
+    "type": "SPOT",
+    "size": 0,
+    "position": 0,
+    "vwap": 0,
+    "nlv": 0,
+    "last": 0,
+    "bid": 0,
+    "bid_size": 0,
+    "ask": 0,
+    "ask_size": 0,
+    "volume": 0,
+    "unrealized": 0,
+    "realized": 0,
+    "currency": "string",
+    "total_volume": 0,
+    "limits": [
+      {
+        "name": "string",
+        "units": 0
+      }
+    ],
+    "interest_rate": 0,
+    "is_tradeable": true,
+    "is_shortable": true,
+    "start_period": 0,
+    "stop_period": 0
+  }
+]
+
+
+Parameters for the securities GET HTTP request
+- ticker* required string   (query)
+- period number             (query)
+
+'''
+
 import requests
 
 host_url = 'http://localhost:9999'          # Make sure the RIT client uses the same 9999 port
