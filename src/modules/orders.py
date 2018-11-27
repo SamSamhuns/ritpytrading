8e6974b5b2419d0480963c38692557b770cb2cf3
SamSamhuns <sthapuskar@yahoo.com>
2018-11-26 22:43:05 -0500

    Added Order Class. Moved main comments to the top.

--- src/modules/orders.py
+++ src/modules/orders.py
@@ -1,3 +1,23 @@
+'''
+order return object attributes
+param possible order attributes: JSON formatted
+i.e. get_order_response( ses, url_end, param="order_id" )
+{
+    "order_id": 1221,
+    "period": 1,
+    "tick": 10,
+    "trader_id": "trader49",
+    "ticker": "CRZY",
+    "type": "LIMIT",
+    "quantity": 100,
+    "action": "BUY",
+    "price": 14.21,
+    "quantity_filled": 10,
+    "vwap": 14.21,
+    "status": "OPEN"
+}
+'''
+
 import requests
 
 # Make sure the RIT client uses the same 9999 port
@@ -10,6 +30,26 @@ base_url = host_url + base_path
 
 class ApiException(Exception):
     pass
+
+
+class Order():
+    def __init__(self, order_response):
+        self.order_id = zz["order_id"]
+        self.period = zz["period"]
+        self.tick = zz["tick"]
+        self.trader_id = zz["trader_id"]
+        self.ticker = zz["ticker"]
+        self.type = zz["type"]
+        self.quantity = zz["quantity"]
+        self.action = zz["action"]
+        self.price = zz["price"]
+        self.quantity_filled = ["quantity_filled"]
+        self.vwap = ["vwap"]
+        self.status = ["status"]
+
+    def __repr__(self)
+    return self.action + '_' +  self.quantity + '_' + self.ticker + '_' + self.price + '__' + self.order_id
+
 
 # function requires a requests.Session() object as the ses argument with a loaded API_KEY
 # order status can be OPEN, TRANSACTED or CANCELLED
@@ -41,33 +81,16 @@ def get_order_response(ses, url_end, param, order_status='OPEN', order_id=None, 
     raise ApiException('Authorization Error: Please check API key.')
 
 
-'''
-order return object attributes
-param possible order attributes: JSON formatted
-i.e. get_order_response( ses, url_end, param="order_id" )
-{
-    "order_id": 1221,
-    "period": 1,
-    "tick": 10,
-    "trader_id": "trader49",
-    "ticker": "CRZY",
-    "type": "LIMIT",
-    "quantity": 100,
-    "action": "BUY",
-    "price": 14.21,
-    "quantity_filled": 10,
-    "vwap": 14.21,
-    "status": "OPEN"
-}
-'''
 # status can be OPEN, TRANSACTED or CLOSED
 # status OPEN by default
 # returns one attribute of an order with entered id or of all orders
 
 
 def get_order_attribute(ses, param, id=None, status='OPEN'):
+    # if no id is entered for the order
     if id == None:
         return get_order_response(ses, '/orders', param, status)
+    # if id is entered for the order
     elif id != None:
         return get_order_response(ses, '/orders/{}', param, order_id=id, status)
 
