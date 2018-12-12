dc07dbfa0cc9ed1d5ce25d054b562e8322f36556
SamSamhuns <sthapuskar@yahoo.com>
2018-12-12 00:27:00 -0500

    Created skeleton test_assets.py file.

---
+++ test/test_assets.py
@@ -0,0 +1,16 @@
+from ritpytrading import assets
+import unittest
+
+class TestAssetsMethods( unittest.TestCase ):
+
+    def test_asset(self):
+        pass
+
+    def test_assets_dict(self):
+        pass
+
+    def test_assets_json(self):
+        pass
+
+if __name__ = "__main__":
+    unittest.main()
