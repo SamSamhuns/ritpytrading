66ea4685e4d4c438d7cb7f85a5d9f966feca0387
SamSamhuns <sthapuskar@yahoo.com>
2018-12-12 03:22:59 -0500

    Changed sample_json_resp to a global var accessible by all test methods

--- test/test_assets.py
+++ test/test_assets.py
@@ -1,49 +1,50 @@
 from ..ritpytrading import assets
 import unittest
 
+_sample_json_resp = [
+    {
+        "ticker": "AAPL",
+        "type": "equity",
+        "description": "Apple Inc",
+        "total_quantity": 10000,
+        "available_quantity": 5000,
+        "lease_price": 120,
+        "convert_from": [
+            {
+                "ticker": "string",
+                "quantity": 0
+            }
+        ],
+        "convert_to": [
+            {
+                "ticker": "string",
+                "quantity": 0
+            }
+        ],
+        "containment": {
+            "ticker": "string",
+            "quantity": 0
+        },
+        "ticks_per_conversion": 0,
+        "ticks_per_lease": 0,
+        "is_available": True,
+        "start_period": 0,
+        "stop_period": 0
+    }
+]
 
 class TestAssetsMethods(unittest.TestCase):
 
     def test_asset(self):
-        sample_json_resp = [
-            {
-                "ticker": "AAPL",
-                "type": "equity",
-                "description": "Apple Inc",
-                "total_quantity": 10000,
-                "available_quantity": 5000,
-                "lease_price": 120,
-                "convert_from": [
-                    {
-                        "ticker": "string",
-                        "quantity": 0
-                    }
-                ],
-                "convert_to": [
-                    {
-                        "ticker": "string",
-                        "quantity": 0
-                    }
-                ],
-                "containment": {
-                    "ticker": "string",
-                    "quantity": 0
-                },
-                "ticks_per_conversion": 0,
-                "ticks_per_lease": 0,
-                "is_available": True,
-                "start_period": 0,
-                "stop_period": 0
-            }
-        ]
-        self.assertEqual(sample_json_resp, assets.assets_response_handle(
-            sample_json_resp, ticker='AAPL'))
+        self.assertEqual(_sample_json_resp, assets.assets_response_handle(
+            _sample_json_resp, ticker='AAPL'))
 
     def test_assets_dict(self):
         pass
 
     def test_assets_json(self):
         pass
+        # self.assertEqual( )
 
 
 if __name__ == "__main__":
