9a0ee347f7e1e88a197664bb968c7ace859d101e
Samridha_Shrestha <SamSamhuns@users.noreply.github.com>
2018-12-26 11:11:37 +0545

    bat script for python artifacts clean

--- windows_batch_scripts/clean_pyc.bat
+++ windows_batch_scripts/clean_pyc.bat
@@ -1,7 +1,6 @@
 @echo off
 Rem Equivalent for clean-pyc:  ## remove Python file artifacts
 
-Rem for /d %%G in ("*.egg-info") do rmdir /s /q "%%~G";
 del /S /F /Q *.pyc >NUL;
 del /S /F /Q *.pyo >NUL;
 del /S /F /Q *~ >NUL;
