4561e0babccba01ff1fc1541071f979cdff18cef
Samridha_Shrestha <SamSamhuns@users.noreply.github.com>
2018-12-26 11:12:12 +0545

    bat script for removing test artifacts

--- windows_batch_scripts/clean_test.bat
+++ windows_batch_scripts/clean_test.bat
@@ -1,6 +1,5 @@
 @echo off
 Rem Equivalent for clean-test: ## remove test and coverage artifacts
-Rem for /d %%G in ("*.egg-info") do rmdir /s /q "%%~G";
 
 if exist .tox\ rmdir /S /Q .tox\ >NUL;
 if exist htmlcov\ rmdir /S /Q htmlcov\ >NUL;
